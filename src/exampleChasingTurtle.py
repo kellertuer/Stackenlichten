e213e3b6c418dc14ff6501e2e88b209f797af056
Ronny Bergmann <mail@rbergmann.info>
2017-05-21 21:23:15 +0200

    fixes a bug in graph21. And adds an example illustrating the turle viz mode.

---
+++ src/exampleChasingTurtle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sys 
+from graph import Graph
+from pixel import Pixel
+from Stackenlichten import *
+from algorithm import *
+from control import *
+
+def run(argv):
+    slc = PyTurtleSLC(60);
+    graph = Graph.load("graphs/graph21.txt")
+    malg = multAlgorithm([
+        AlgBackground([0.75,0.5625,.0],graph.clone()),
+        AlgTrigWalkCycle(1,[90,330,90,210],[1,1,1,1,1],graph.clone())
+        ],graph.clone())
+    malg2 = multAlgorithm([
+        AlgBackground([0.1875,.0,0.75],graph.clone()),
+        AlgTrigWalkCycle(19,[90,330,90,210],[1,1,1,1,1],graph.clone())
+        ],graph.clone())
+    aAlg = addAlgorithm([AlgBackground([0,0,0],graph.clone()),malg,malg2],graph)
+    alg = mainAlgorithm(slc,aAlg)
+    c = SimpleControl(alg,{'frameratre':30});
+    c.start()
+    
+if __name__ == "__main__":
+        run(sys.argv[1:])
